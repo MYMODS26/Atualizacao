d6d1204f4e60e9d30ab2cd9ffed32121b94b146b
MYMODS26 <96635813+MYMODS26@users.noreply.github.com>
2023-06-29 16:34:28 -0300

    Create MyMods.js

---
+++ MyMods.js
@@ -0,0 +1,11 @@
+{
+	"version": "22.1",
+	"download": "https://youtube.com/@Maycon_Mods",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/MYMODS26/Atualizacao/main/MyMods_Changelog.html"
+		}
+
+	]
+}
