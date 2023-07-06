1a06a762a772bf3bdb79a677c8c68c241f640aac
MYMODS26 <96635813+MYMODS26@users.noreply.github.com>
2023-07-06 12:17:39 -0300

    Create MyMods_COR.js

---
+++ MyMods_COR.js
@@ -0,0 +1,11 @@
+{
+	"version": "22.2",
+	"download": "https://youtube.com/@Maycon_Mods",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/MYMODS26/Atualizacao/main/MyMods_COR_Changelog.html"
+		}
+
+	]
+}
